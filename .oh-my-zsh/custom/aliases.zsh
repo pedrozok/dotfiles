7aed3d3597aa5a23e4affc3c5834519e623633e0
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2023-12-29 16:13:25 +0000

    add mvim cmd

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,3 +3,4 @@ alias zshedit="vim ~/.zshrc"
 alias brewdeps="brew deps --installed --tree"
 alias gitclean="gco master && ggpull && git fetch && git branch | grep -v "master" | xargs git branch -D" # checkout to master, pull master, fetch and remove all other local branches except master
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew' # Fix brew error due to pyenv
+alias mvim='/Applications/MacVim.app/Contents/bin/gvim'
