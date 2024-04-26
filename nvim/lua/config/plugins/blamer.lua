8612f523223bb6a865d12a8c4aa2c628baaae047
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2024-04-26 19:41:49 +0100

    initial commit

---
+++ nvim/lua/config/plugins/blamer.lua
@@ -0,0 +1,7 @@
+return {
+  "APZelos/blamer.nvim",
+  config = function()
+    vim.g.blamer_enabled = 1
+    vim.g.blamer_show_in_insert_modes = 0
+  end
+}
---
+++ nvim/lua/config/plugins/nvimtree.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  config = function()
+    local api = require "nvim-tree.api"
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    -- optionally enable 24-bit colour
+    vim.opt.termguicolors = true
+    require("nvim-tree").setup()
+    -- toggle tree
+    vim.keymap.set('n', '<C-f>', ':NvimTreeToggle<CR>')
+  end
+}
