775e6e272d25952df822b6381f9a52d2963fdadc
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2024-04-28 00:50:28 +0100

    feat: add keymaps for tab navigation

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -10,3 +10,7 @@ vim.keymap.set("n", '<Esc><Esc>', ':w<CR>')
 -- shift selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- tab navigation
+vim.keymap.set("n", "H", "gT")
+vim.keymap.set("n", "L", "gt")
