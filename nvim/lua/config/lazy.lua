4e778966733460ec46d4bdf483405551ca225540
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2024-06-28 18:26:21 +0100

    feat: added copilot chat

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
