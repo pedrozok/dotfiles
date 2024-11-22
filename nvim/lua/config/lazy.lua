d79c5a57916e0d1c2c6feeea13d44fd6d1748004
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2024-11-22 15:38:51 +0000

    update copilot package import

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,8 +21,8 @@ require("lazy").setup({
       },
     },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
