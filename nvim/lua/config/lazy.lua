02b59de468c1b4afd83c7b921a2b711c98f5bad9
Pedro Landeiroto <p.landeiroto@runtime-revolution.com>
2025-01-05 11:27:44 +0000

    feat: enabled mini-surround

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
       },
     },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
